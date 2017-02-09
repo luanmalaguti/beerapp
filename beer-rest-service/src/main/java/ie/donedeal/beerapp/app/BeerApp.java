8cef8eae4eacce99a590924a8c573f0ed7b1eb0e
Luan Malaguti <luanmalaguti@gmail.com>
2017-02-09 00:39:54 +0000

    Design improvements

--- beer-rest-service/src/main/java/ie/donedeal/beerapp/app/BeerApp.java
+++ beer-rest-service/src/main/java/ie/donedeal/beerapp/app/BeerApp.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
  * Application executable that uses a Tomcat embedded servlet to provide a standalone application 
  * 
  * 		<p>How to execute?</p>
- * 		<p>Option 1. <code>java -jar Application</code></p>
- * 		<p>Option 2. Run as > Java Application</p>
+ * 		<p>Option 1. <code>java -jar beer-rest-service.jar</code></p>
+ * 		<p>Option 2. BeerApp.java > Run as > Java Application</p>
  * 
  * @author Luan Reffatti
  */
